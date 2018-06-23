3c7bb4669060334b4841be173fab6e6f3fa19ec2
Burhanuddin Rashid <burhanrashid52@gmail.com>
2018-06-23 15:02:50 +0530

    Column Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sample_flutter_app/button_widget.dart';
 
 void main() => runApp(new MyApp());
 
@@ -12,7 +11,32 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Controller(),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("LinearLayout Example"),
+        ),
+        body: new Container(
+          color: Colors.yellowAccent,
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              new Icon(
+                Icons.access_time,
+                size: 50.0,
+              ),
+              new Icon(
+                Icons.pie_chart,
+                size: 100.0,
+              ),
+              new Icon(
+                Icons.email,
+                size: 50.0,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
